8fab88a21d6cf5dbe3c19a77f3e38c6b2d0a528e
Lily Keeling <lily.keeling@gmail.com>
2019-05-01 10:04:42 +0100

    testing JS and updated design/layout

--- js/main.js
+++ js/main.js
@@ -1,6 +1,99 @@
 
 
-document.getElementById("curlew").style.opacity = 1
+
+//let bird = document.getElementById("lapwing-image");
+
+//const playAudio = () => {
+
+
+//bird.addEventListener('click', playAudio)
+
+ //const testFunction = () => {
+//let audio = new Audio("bird-sounds/lapwing-sound.mp3" )
+//const playAudio = () => {
+	//	audio.play()
+	//}
+
+//let nodeImages =	document.querySelectorAll(('[class^="bird-image"]'))
+//let birdImageArray = Array.prototype.slice.call(nodeImages)
+//let birdImageArray = [];
+//birdImageArray.push(document.querySelectorAll(('[class^="bird-image"]')))
+//birdImageArray[2].addEventListener('click', playAudio)
+
+//for(i=0;i<birdImageArray; i++){
+//birdImageArray[i].addEventListener('click', playAudio)
+//birdImageArray.forEach(function(birdImage){
+//	birdImage.addEventListener('click', playAudio)
+//})
+
+
+//}
+//testFunction()
+
+const handleBirdClick = function(event) {
+	let birdWordTest = new RegExp("image")
+	const parentElement = event.target.parentElement;
+	console.log(parentElement.id)
+	if (birdWordTest.test(parentElement.id)) {
+
+		
+	}
+}
+
+document.addEventListener("click", handleBirdClick)
+
+/*const clickToPlay = () =>{
+
+	let nodeBirds =	document.querySelectorAll(('[class^="bird-image"]'))
+let birdImageArray = Array.prototype.slice.call(nodeBirds)
+	//let birdImageArray = [];
+const getBirdName = () => {
+	//let nodeBirds =	document.querySelectorAll(('[class^="bird-image"]'))
+	let birdsArray = Array.prototype.slice.call(nodeBirds)
+let classNameString = ''
+let birdWord = ''
+	for (i=0;i<birdsArray.length;i++) {
+		classNameString = birdsArray[i].className.animVal.split(' ').toString().split('-')[2]
+		console.log(classNameString)
+}
+}
+birdImageArray.forEach(function(birdImage){
+	birdImage.addEventListener('click', getBirdName)
+})
+	//let birdName = birdsArray.forEach(function(bird){
+	//	return bird.className
+//	})
+}
+clickToPlay()*/
+
+
+
+//	let idArray = [];
+//	let getClassName = () =>{
+//	return bird.className
+//}
+//	const getNameString=()=>{
+		//should return array of
+	//	let classArray = (array)={
+	//		birdsArray.split(',')
+	//		}
+//		return classArray[0]
+//	}
+
+//
+//const birdSoangObject= {
+//	birdName: cuckoo
+//}
+
+
+
+
+/*let cuckooSound = new Audio("bird-sounds/cuckoo-sound.mp3")
+const playCuckoo =()=>{
+	 cuckooSound.play()
+}
+document.getElementById('cuckoo-image').addEventListener('click', playCuckoo)
+
 
 
 
@@ -19,4 +112,4 @@ const toggleVisibility = function(el) {
 	}
 }
 
-document.addEventListener("click", handleFragmentClick)
+document.addEventListener("click", handleFragmentClick)*/
